9db509c122422dea73ecc62ef325fba4f4ee0f33
Pradip Mehta - PM AM <pradip.mehta13@gmail.com>
2020-11-04 00:00:00 +0530

    Add Authenticate code

--- api/modules/products/index.js
+++ api/modules/products/index.js
@@ -5,11 +5,12 @@ var router = express.Router();
 var ctrlProduct = require("./products.ctrl");
 var dirname = path.basename(__dirname);
 var filename = dirname + "." + path.basename(__filename);
+var auth = require("../../config/auth");
 var filelogger = require("../../config/server-logger").filelogger(
   dirname,
   filename
 );
 
-router.route("/products").get(ctrlProduct.GetProducts);
+router.route("/products").get(auth.authenticate,ctrlProduct.GetProducts);
 
 module.exports = router;
